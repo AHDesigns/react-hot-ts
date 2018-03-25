82498f7d6d57773b7539aa350e7d692b65b4f1ef
Adam <adam.hall@sky.uk>
2018-03-25 20:19:40 +0100

    hot reloading
    hot reloading
    
    t

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.jsx';
+// import './styles/global.scss';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('wrapper'),
+);
