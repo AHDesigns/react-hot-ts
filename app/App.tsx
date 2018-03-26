46133e183825b1450c1991c45bf097cb3b6745de
Adam Hall <adamhalldesigns@gmail.com>
2018-03-26 15:10:18 +0100

    tidy

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import { hot } from 'react-hot-loader';
+import * as React from "react";
+import { hot } from "react-hot-loader";
 
 const App = () => (
   <h1>
     Hello, you.<br />
   </h1>
-)
-console.log('chuffty');
+);
+// console.log('chuffty');
 
-export default hot(module)(App)
+export default hot(module)(App);
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
-import { render } from 'react-dom'
-import App from './App'
+import * as React from "react";
+import { render } from "react-dom";
+import App from "./App";
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-console.log('hi');
+const root = document.createElement("div");
+document.body.appendChild(root);
+// console.log('hi');
 
-render(<App />, root)
+render(<App />, root);
